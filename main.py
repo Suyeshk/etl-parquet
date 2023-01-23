11ad2ae57675358b6d7a2b8fcacfde4e6c96cd68
Suyeshk <109844935+Suyeshk@users.noreply.github.com>
2023-01-23 10:56:29 +0530

    Create main.py

---
+++ main.py
@@ -0,0 +1,31 @@
+# Import the necessary libraries
+import pandas as pd
+import boto3
+from io import BytesIO
+
+# Create an S3 client
+s3 = boto3.client('s3')
+
+# Set the source and destination bucket names
+src_bucket_name = 'bucketsnowflakes47'
+dst_bucket_name = 'snowoutput'
+
+# Set the source and destination keys (file names)
+src_key = 'csv/data.csv'
+dst_key = 'parquet/data1.parquet'
+
+# Use the S3 client to download the object
+response = s3.get_object(Bucket=src_bucket_name, Key=src_key)
+data = response['Body'].read()
+
+# Convert the downloaded bytes to a DataFrame
+df = pd.read_csv(BytesIO(data))
+
+# Add a new column to the DataFrame
+df['new_column'] = 'default_value'
+
+# Convert the DataFrame to a Parquet file and upload it to S3
+buf = BytesIO()
+df.to_parquet(buf, index=False)
+buf.seek(0)
+s3.upload_fileobj(buf, dst_bucket_name, dst_key)
